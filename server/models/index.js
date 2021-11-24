397b86ee8a5387a89db6615b80bccc605f8c94a7
unknown <golden.star0228@gmail.com>
2021-11-24 10:04:43 -0500

    update db config

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,25 @@
 const Sequelize = require('sequelize')
+const config = require('../config')
 
-const connectionSeq = new Sequelize('braindance', 'root', '', {
-  host: 'localhost',
+console.log("config: ", process.env.NODE_ENV)
+let dbConfig = {}
+
+if (process.env.NODE_ENV === 'development') {
+  dbConfig = {
+    user: 'root',
+    password: '',
+    host: '127.0.0.1'
+  }
+} else {
+  dbConfig = {
+    user: 'admin',
+    password: 'dky#$55Khmdhj009p',
+    host: 'mint-braindance-gg-db.c7qko9k9lqby.us-west-2.rds.amazonaws.com'
+  }
+}
+
+const connectionSeq = new Sequelize('braindance', dbConfig.user, dbConfig.password, {
+  host: dbConfig.host,
   dialect: 'mysql',
   port: 3306,
 })
