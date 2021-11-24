885beb1396ed194a24ac17b65e9782f3657611ce
unknown <golden.star0228@gmail.com>
2021-11-23 21:47:29 -0500

    updated host

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('./services/express')
 const routes = require('./routes')
 const db = require('./models')
 
-const port = process.env.PORT || 4000
+const port = 4000
+const host = '127.0.0.1'
 const app = express(routes)
 
 db.connectionSeq.sync()
 
-app.listen(port, (err) => {
+app.listen(port, host, (err) => {
   if (err) throw err
   console.log(`> Ready on http://localhost:${port}`)
 })
